e31543a3e6cdf76859ded898c714c68fb712ea36
Vikas- <42265144+vikasyadav17@users.noreply.github.com>
2022-06-13 21:53:09 +0530

    Mirror a binary tree

--- src/binaryTrees/MirrorABinaryTreeUse.java
+++ src/binaryTrees/MirrorABinaryTreeUse.java
@@ -2,16 +2,25 @@ package binaryTrees;
 
 public class MirrorABinaryTreeUse {
 
-	public static void mirrorBinaryTree(BinaryTreeNode<Integer> root){
-		//Your code goes here
-		if(root==null) {
-			return ;
+	public static BinaryTreeNode mirror(BinaryTreeNode<Integer> root) {
+		if (root == null) {
+			return null;
 		}
-		
-		mirrorBinaryTree(root.right);
-		mirrorBinaryTree(root.left);
-		//System.out.println();
+
+		BinaryTreeNode<Integer> left = mirror(root.left);
+		BinaryTreeNode<Integer> right = mirror(root.right);
+
+		root.left = right;
+		root.right = left;
+
+		return root;
 	}
-	
-	
+
+	public static void mirrorBinaryTree(BinaryTreeNode<Integer> root) {
+		// Your code goes here
+
+		mirror(root);
+
+	}
+
 }
