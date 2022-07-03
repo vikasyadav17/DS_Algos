1e2fdce7dc2e24c418f8a48538354d2519568ae4
Vikas- <42265144+vikasyadav17@users.noreply.github.com>
2022-07-03 19:10:09 +0530

    last question of linkedlist 2

--- src/linkedListTwo/DeleteEveryNNodes.java
+++ src/linkedListTwo/DeleteEveryNNodes.java
@@ -5,60 +5,57 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class DeleteEveryNNodes {
-	
-	   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    
-	    public static LinkedListNode<Integer> takeInput() throws IOException {
-	        LinkedListNode<Integer> head = null, tail = null;
 
-	        String[] datas = br.readLine().trim().split("\\s");
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	        int i = 0;
-	        while(i < datas.length && !datas[i].equals("-1")) {
-	            int data = Integer.parseInt(datas[i]);
-	            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
-	            if(head == null) {
-	                head = newNode;
-	                tail = newNode;
-	            }
-	            else {
-	                tail.next = newNode;
-	                tail = newNode;
-	            }
-	            i += 1;
-	        }
+	public static LinkedListNode<Integer> takeInput() throws IOException {
+		LinkedListNode<Integer> head = null, tail = null;
 
-	        return head;
-	    }
-	    
-	    public static void print(LinkedListNode<Integer> head){
-	        while(head != null) {
-	            System.out.print(head.data + " ");
-	            head = head.next;
-	        }
-	        
-	        System.out.println();
-	    }
-	    
-	    public static void main(String[] args) throws NumberFormatException, IOException {
-	        
-	        int t = Integer.parseInt(br.readLine().trim());
+		String[] datas = br.readLine().trim().split("\\s");
 
-	        while (t > 0) {
-	            
-	            LinkedListNode<Integer> head = takeInput();
-	            String[] m_n = br.readLine().trim().split("\\s");
+		int i = 0;
+		while (i < datas.length && !datas[i].equals("-1")) {
+			int data = Integer.parseInt(datas[i]);
+			LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+			if (head == null) {
+				head = newNode;
+				tail = newNode;
+			} else {
+				tail.next = newNode;
+				tail = newNode;
+			}
+			i += 1;
+		}
 
-	            int m = Integer.parseInt(m_n[0]);
-	            int n = Integer.parseInt(m_n[1]);
-
-	            LinkedListNode<Integer> newHead = DeleteEveryNNodesUse.skipMdeleteN(head, m, n);
-	            print(newHead);
-	            
-	            t -= 1;
-	        }
-
-	    }
+		return head;
 	}
 
+	public static void print(LinkedListNode<Integer> head) {
+		while (head != null) {
+			System.out.print(head.data + " ");
+			head = head.next;
+		}
+
+		System.out.println();
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		int t = Integer.parseInt(br.readLine().trim());
+
+		while (t > 0) {
+
+			LinkedListNode<Integer> head = takeInput();
+			String[] m_n = br.readLine().trim().split("\\s");
+
+			int m = Integer.parseInt(m_n[0]);
+			int n = Integer.parseInt(m_n[1]);
+
+			LinkedListNode<Integer> newHead = DeleteEveryNNodesUse.skipMdeleteN(head, m, n);
+			print(newHead);
+
+			t -= 1;
+		}
+
+	}
 }
--- src/linkedListTwo/DeleteEveryNNodesUse.java
+++ src/linkedListTwo/DeleteEveryNNodesUse.java
@@ -11,12 +11,25 @@ public class DeleteEveryNNodesUse {
 		if (N == 0) {
 			return head;
 		}
-
+		int i = 1, j = 0;
 		LinkedListNode<Integer> node = head;
-		while (node == null) {
-			
-			
+		while (node != null) {
+			if (i == M) {
+				LinkedListNode<Integer> nodeForSkipping = node;
+				while (j != N && nodeForSkipping.next != null) {
+					++j;
+					nodeForSkipping = nodeForSkipping.next;
+				}
+				node.next = nodeForSkipping.next;
+				j = 0;
+				i = 0;
+			} else {
+				i++;
+				node = node.next;
+			}
+
 		}
+		return head;
 
 	}
 
